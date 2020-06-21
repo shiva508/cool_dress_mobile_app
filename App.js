75c0a70cf428156efa6371cbbf179a7fff5d940c
shiva <dasarishiva1@gmail.com>
2020-06-21 12:29:33 +0530

    Tst
    Adfdf

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Cool Dress App</Text>
+      <Text>Cool Dress </Text>
     </View>
   );
 }
