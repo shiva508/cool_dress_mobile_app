1316dfadc8d86faae8d22de12176cd0060f52816
shiva <dasarishiva1@gmail.com>
2020-06-21 12:09:15 +0530

    Update App.js
    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wellcome test</Text>
+      <Text>Cool Dress App</Text>
     </View>
   );
 }
